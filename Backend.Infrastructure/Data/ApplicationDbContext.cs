a746f331740c86f7f01ae8eceeb8de6b56704ba2
JeanProvost <jprov310@gmail.com>
2026-03-25 16:35:54 -0600

    Model create

--- Backend.Infrastructure/Data/ApplicationDbContext.cs
+++ Backend.Infrastructure/Data/ApplicationDbContext.cs
@@ -12,5 +12,11 @@ namespace Backend.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
     }
 }
