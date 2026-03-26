08db2573a3e61eafbba3a29cfe8c04a550fd35f7
JeanProvost <jprov310@gmail.com>
2026-03-25 19:44:35 -0600

    Creating `BaseEntity`

---
+++ Backend.Core/Entities/Base/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Entities.Base
+{
+    public class BaseEntity<Tid>
+    {
+        public BaseEntity()
+        {
+            Id = default!;
+        }
+
+        [Key]
+        public Tid Id { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
