5055ff28516d424944dec3b21918ecfd9d9510c7
JeanProvost <jprov310@gmail.com>
2026-04-14 19:09:28 -0600

    User controller

---
+++ Backend.API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        //private readonly IUserService _userService;
+
+        public UserController()
+        {
+
+        }
+    }
+}
