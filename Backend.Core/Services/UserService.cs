907cd8f52151ab4034a2cd6d77325fe6f6cb5572
JeanProvost <jprov310@gmail.com>
2026-04-14 19:10:21 -0600

    User controller/services setup

---
+++ Backend.Core/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Core.Interfaces.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Core.Services
+{
+    public class UserService : IUserService
+    {
+    }
+}
