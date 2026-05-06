32944ca743538568cf732a53961056f59eb14d7b
JeanProvost <jprov310@gmail.com>
2026-05-05 20:39:31 -0600

    Implementing login service

--- Backend.Core/Entities/Supplement/Persistence/SupplementConfiguration.cs
+++ Backend.Core/Entities/Supplement/Persistence/SupplementConfiguration.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Backend.Core.Entities.Supplement.Persistence
+
 {
     public class SupplementConfiguration : IEntityTypeConfiguration<Supplement>
     {
