75529de41dd6e9310212c515adec0a35ed9cf555
JeanProvost <jprov310@gmail.com>
2026-05-17 13:10:29 -0600

    Adding `UserStackEntry` DbSet

--- Backend.Infrastructure/Data/ApplicationDbContext.cs
+++ Backend.Infrastructure/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Backend.Core.Entities.Supplements;
 using Backend.Core.Entities.Users;
 using Backend.Core.Entities.Users.Persistence;
+using Backend.Core.Entities.UserStackEntries;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Infrastructure.Data
@@ -25,5 +26,6 @@ namespace Backend.Infrastructure.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<Supplement> Supplements { get; set; }
+        public DbSet<UserStackEntry> UserStackEntries { get; set; }
     }
 }
