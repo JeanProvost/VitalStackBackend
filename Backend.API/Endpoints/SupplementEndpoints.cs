045912e19b2a95f707ca62e913e9549e333468f6
JeanProvost <jprov310@gmail.com>
2026-05-19 21:06:05 -0600

    Add supplement endpoint

--- Backend.API/Endpoints/SupplementEndpoints.cs
+++ Backend.API/Endpoints/SupplementEndpoints.cs
@@ -1,7 +1,10 @@
-﻿using Backend.Infrastructure.Data;
+﻿using Backend.Core.Entities.UserStackEntries;
+using Backend.Infrastructure.Data;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace Backend.API.Endpoints
 {
@@ -9,6 +12,7 @@ namespace Backend.API.Endpoints
     {
         public static RouteGroupBuilder MapSupplementEndpoints(this RouteGroupBuilder group)
         {
+            //Search Supplement catalog
             group.MapGet("/search", async (
                 [FromQuery] string query,
                 ApplicationDbContext _db) =>
@@ -28,7 +32,49 @@ namespace Backend.API.Endpoints
                 return Results.Ok(results);
             });
 
+            //Add Supplement to User Stack
+            group.MapPost("/stack", async (
+                [FromBody] AddToStackRequest request,
+                ApplicationDbContext _db,
+                ClaimsPrincipal user) =>
+            {
+                var userId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+                if (request.MasterSupplementId == null && string.IsNullOrWhiteSpace(request.CustomName))
+                {
+                    return Results.BadRequest("CustomerName and MasterSupplementId required");
+                }
+
+                var newEntry = new UserStackEntry
+                {
+                    UserId = userId,
+                    MasterSupplementId = request.MasterSupplementId,
+                    CustomName = request.CustomName,
+                    Cusomization = new StackCustomization
+                    {
+                        Form = request.Form,
+                        Dosage = request.Dosage,
+                        Brand = request.Brand,
+                        TimeOfDayTarget = request.TimeOfDayTarget
+                    }
+                };
+
+                _db.UserStackEntries.Add(newEntry);
+                await _db.SaveChangesAsync();
+
+                return Results.Created($"/api/supplements/stack/{newEntry.Id}", newEntry);
+            });
+
             return group;
         }
+
+        public record AddToStackRequest(
+            Guid? MasterSupplementId,
+            string? CustomName,
+            string? Form,
+            string? Dosage,
+            string? Brand,
+            string? TimeOfDayTarget
+        );
     }
 }
